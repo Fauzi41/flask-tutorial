10533798691ea68e980635d58dfa0fc78a4cbe21
Fauzi41 <anasfauzi1997@gmail.com>
2022-04-27 17:45:43 +0700

    Chapter 2: Templates

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World"
+    user = {'username': 'Anas'}
+    posts = [
+        {
+            'author': {'username': 'Fauzi'},
+            'body': 'Beautiful day in Kudus!'
+        },
+        {
+            'author': {'username': 'Tom'},
+            'body': 'Beautiful day in Bali!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
